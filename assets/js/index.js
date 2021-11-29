0f4d30c360772245df4a60ce819267fc522bd2c5
n0ss4 <nghazouani@outlook.com>
2021-11-29 14:31:30 +0100

    Fontawesome and requireds from

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,24 @@ function guardardades(){
     miLocalStorage.setItem('name', JSON.stringify(name));
     miLocalStorage.setItem('date', JSON.stringify(date));
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
