a585fffba5e95abcce881ac7cd9244decbd694b3
David Ayachi Matas <divad.davidam@gmail.com>
2021-12-02 22:11:12 +0100

    Moficacions Generals

--- assets/js/functionsCarrito.js
+++ assets/js/functionsCarrito.js
@@ -3,7 +3,7 @@
     var total = 0;
     const DOMcarrito = document.querySelector('#carrito');
     const DOMtotal = document.querySelector('#total');
-    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    //const DOMbotonVaciar = document.querySelector('#boton-vaciar');
     const miLocalStorage = window.localStorage;
 
     function anyadirProductoAlCarrito(evento) {
@@ -16,32 +16,58 @@
         }else{
             console.log({evento_target: evento.target});
         }
-
-        
     }
 
     function renderizarCarrito() {
+        var total=0;
         DOMcarrito.textContent = '';
         const carritoSinDuplicados = [...new Set(carrito)];
         carritoSinDuplicados.forEach((item) => {
+            total++;
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
                 return itemId === item ? total += 1 : total;
             }, 0);
-            /*const miNodo = document.createElement('li');
-            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}€`;
+
+            const li =document.createElement('li');
+            li.classList.add('clearfix')
+
+            const img = document.createElement('img');
+            img.setAttribute('alt','item1');
+            img.style.width='30px';
+            img.style.height='30px';
+            img.setAttribute('src',miItem[0].imagen);
+
+            const nom=document.createElement('span');
+            nom.classList.add('item-name');
+            nom.textContent=`${miItem[0].nombre}`;
+
+            const preu=document.createElement('span');
+            preu.classList.add('item-name');
+            preu.textContent=`${miItem[0].precio}€`;
+
+            const quantitat=document.createElement('span');
+            quantitat.classList.add('item-name');
+            quantitat.textContent=`Quantity: ${numeroUnidadesItem}`;
+
             const miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
             miBoton.dataset.item = item;
             miBoton.addEventListener('click', borrarItemCarrito);
-            miNodo.appendChild(miBoton);
-            DOMcarrito.appendChild(miNodo);*/
+            li.appendChild(img);
+            li.appendChild(nom);
+            li.appendChild(preu);
+            li.appendChild(quantitat)
+            li.appendChild(miBoton);
+
+            DOMcarrito.appendChild(li);
         });
+        document.getElementById("contadorProductos").innerHTML = total;
+
     }
 
     function borrarItemCarrito(evento) {
@@ -65,12 +91,12 @@
         DOMtotal.textContent = total.toFixed(2);
     }
 
-    function vaciarCarrito() {
+   /* function vaciarCarrito() {
         carrito = [];
         renderizarCarrito();
         calcularTotal();
         localStorage.removeItem('carrito');
-    }
+    }*/
 
     function guardarCarritoEnLocalStorage () {
         miLocalStorage.setItem('carrito', JSON.stringify(carrito));
@@ -82,7 +108,7 @@
         }
     }
 
-    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+    //DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
     cargarCarritoDeLocalStorage();
     calcularTotal();
